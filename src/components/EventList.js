622a0505dc15234820306571fc9c799486bdd7b3
chrisspenceratx <chrisspenceratx@gmail.com>
2023-11-11 15:58:51 -0600

    testing EventList

--- src/components/EventList.js
+++ src/components/EventList.js
@@ -1,9 +1,13 @@
 // src/components/EventList.js
 
-const EventList = () => {
-  return (
-    <ul id="event-list"></ul>
-  );
+import Event from "./Event";
+
+const EventList = ({ events }) => {
+ return (
+   <ul id="event-list">
+     {events.map(event => <Event event={event} />)}
+   </ul>
+ );
 }
 
 export default EventList;
