e1a3fdd4167ec0695f48e6efb064ec5a5b7fedb6
chrisspenceratx <chrisspenceratx@gmail.com>
2023-12-07 15:49:11 -0600

    makes value of input in query state

--- src/components/CitySearch.js
+++ src/components/CitySearch.js
@@ -10,6 +10,7 @@ const CitySearch = () => {
         type="text"
         className="city"
                 placeholder="Search for a city"
+                value={(query)}
         onFocus={() => setShowSuggestions(true)}
       />
       {showSuggestions ? <ul className="suggestions"></ul> : null}
