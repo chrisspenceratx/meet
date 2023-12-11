d02de6651cff1a595560cd5db7638ae93dbff9ec
chrisspenceratx <chrisspenceratx@gmail.com>
2023-12-11 14:42:47 -0600

    importing new information for 4.5

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 // src/App.js
 
+
+import { useEffect, useState } from 'react';
 import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
+import NumberOfEvents from './components/NumberOfEvents';
+import { getEvents } from './api';
+
 import './App.css';
 
 return (
